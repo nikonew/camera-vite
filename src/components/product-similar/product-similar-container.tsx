c9a1663260c91e07cea2737d49cfaee3dc0778fd
nikonew <spiritofadv@yandex.ru>
2024-10-15 23:52:38 +0300

    add swiper

--- src/components/product-similar/product-similar-container.tsx
+++ src/components/product-similar/product-similar-container.tsx
@@ -5,7 +5,11 @@ import { useEffect } from 'react';
 import { fetchAllSimilar } from '../../store/thunk/thunk';
 import Spinner from '../spinner-coponent/spinner';
 import { similarProductSelectors } from '../../store/slice/similar-slice';
-
+import { Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './product-similar.css';
+import { Navigation } from 'swiper/modules';
 
 export default function ProductSimilarContainer ():JSX.Element {
   const dispatch = useAppDispatch();
@@ -29,11 +33,46 @@ export default function ProductSimilarContainer ():JSX.Element {
       <div className="container">
         <h2 className="title title--h3">Похожие товары</h2>
         <div className="product-similar__slider">
+
           <div className="product-similar__slider-list">
-            {similar.map((item) => (
-              <SimilarItem key={item.id} item={item}/>
-            ))}
+            <button className="slider-controls slider-controls--prev"
+              type="button"
+              aria-label="Предыдущий слайд"
+              style={{pointerEvents: 'auto'}}
+              disabled
+            >
+              <svg width="7" height="12" aria-hidden="true">
+                <use xlinkHref="#icon-arrow"></use>
+              </svg>
+            </button>
+            <Swiper
+              slidesPerView={3}
+              slidesPerGroup={3}
+              spaceBetween={30}
+              modules={[Navigation]}
+              navigation={{
+                prevEl: '.slider-controls--prev',
+                nextEl: '.slider-controls--next'
+              }}
+            >
+              {similar.map((item) => (
+                <SwiperSlide key={item.id}>
+                  <SimilarItem key={item.id} item={item}/>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
+          <button
+            className="slider-controls slider-controls--next"
+            type="button"
+            aria-label="Следующий слайд"
+            style={{pointerEvents: 'auto'}}
+          >
+            <svg width="7" height="12" aria-hidden="true">
+              <use xlinkHref="#icon-arrow"></use>
+            </svg>
+          </button>
+
         </div>
       </div>
     </section>
